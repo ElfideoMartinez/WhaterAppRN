accb7cf3394dbe9698c5216e7273d270e5393470
Daniel Martinez <kyletubman@Kyles-MacBook-Air.local>
2023-03-29 19:48:59 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import MainView from './src/components/MainView';
+
+function App(): JSX.Element {
+  return <MainView />;
+}
+
+export default App;
